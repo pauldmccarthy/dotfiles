cced773e24837fa993877cdc2dcdefcea09995a7
Paul McCarthy <pauldmccarthy@gmail.com>
2019-10-23 14:14:11 +0100

    zsh aliases

--- oh-my-zsh-custom/alias.zsh
+++ oh-my-zsh-custom/alias.zsh
@@ -7,7 +7,7 @@ alias e=$EDITOR
 alias tmux="tmux attach || tmux new"
 alias less="less -RFX"
 alias diff="colordiff"
-alias pyp='export PYTHONPATH=$(pwd):$PYTHONPATH'
+alias pyp='export PYTHONPATH=$(pwd):$PYTHONPATH; echo $PYTHONPATH'
 alias pyclean='find . -name "__pycache__" -exec rm -r "{}" \; ; find . -name "*.pyc" -delete; find . -name "*.pye" -delete'
 alias fslconf=". /home/paulmc/fsl/fslconf.sh"
 alias fedev3=". /home/paulmc/Projects/fsleyes/fsleyes.gtk3.dev.sh; pushd /home/paulmc/Projects/fsleyes"
@@ -15,3 +15,7 @@ alias fedev=".  /home/paulmc/Projects/fsleyes/fsleyes.gtk2.dev.sh; pushd /home/p
 alias fundev=". /home/paulmc/Projects/funpack/funpack_dev.sh; pushd /home/paulmc/Projects/funpack"
 alias workon=". /usr/local/bin/workon"
 alias o="/usr/local/bin/myopen"
+
+# clipcopy/clippaste are zsh functions
+alias clipc="clipcopy"
+alias clipp="clippaste"
